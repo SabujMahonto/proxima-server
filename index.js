a5269466ec1ed0fa09db327e3db8c8f0d8e26df3
sabujmahonto <sabujmahonto@gmail.com>
2023-08-03 11:50:58 +0600

    mongodb connection added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const mongoose = require("mongoose");
 const projectRoute = require("./routes/project.route");
 // variables
 const port = process.env.PORT || 3000;
@@ -8,13 +9,24 @@ const port = process.env.PORT || 3000;
 // express app
 const app = express();
 //middleware
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 //routes
 app.use("/api/projects", projectRoute);
-
-// listen for request
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+//Mongoose
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for request
+    app.listen(port, () => {
+      console.log(`connection to mongo listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
